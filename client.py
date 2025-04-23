17907a2678e576e331218957b42313fb26f19241
red <red.avtovo@gmail.com>
2025-04-23 23:54:12 +0200

    client bugfix

--- client.py
+++ client.py
@@ -10,7 +10,6 @@ import telnetlib
 import subprocess
 import time
 import random
-import signal
 
 # Try to import configuration from config.py if it exists
 try:
@@ -42,8 +41,7 @@ def send_command_to_vlc(command, host, port):
         with telnetlib.Telnet(host, port) as tn:
             tn.read_until(b">", timeout=1)
             tn.write(command.encode("utf-8") + b"\n")
-            # response = tn.read_until(b">", timeout=1).decode("utf-8").strip()
-            response = tn.read_eager().decode("utf-8").strip()
+            response = tn.read_until(b">", timeout=1).decode("utf-8").strip().rstrip("> ").strip()
             print(f"VLC command {command} response: {response}")
             return response
     except Exception as e:
@@ -71,7 +69,7 @@ def start_vlc(video_path, host, port):
     return vlc_process
 
 
-def get_video_duration(host, port):
+def get_video_duration(host = DEFAULT_VLC_CONNECT_HOST, port = DEFAULT_VLC_PORT):
     """Get the duration of the currently playing video in seconds."""
     try:
         response = send_command_to_vlc("get_length", host, port)
@@ -82,7 +80,7 @@ def get_video_duration(host, port):
         return 0
 
 
-def parse_timecode(timecode):
+def parse_timecode(timecode, host = DEFAULT_VLC_CONNECT_HOST, port = DEFAULT_VLC_PORT):
     """Convert timecode format (hh:mm:ss, mm:ss or ss, xx% or -1) to seconds, supporting percentages and -1 for random."""
     # Match hh:mm:ss format
     match = re.match(r'^(\d+):(\d+):(\d+)$', timecode)
@@ -106,12 +104,12 @@ def parse_timecode(timecode):
     match = re.match(r'^(\d+)%$', timecode)
     if match:
         percentage = int(match.group(1))
-        total_duration = get_video_duration(DEFAULT_VLC_CONNECT_HOST, DEFAULT_VLC_PORT)
+        total_duration = get_video_duration(host, port)
         return int(percentage * 0.01 * total_duration)
 
     # Match -1 for random
     if timecode == "-1":
-        total_duration = get_video_duration(DEFAULT_VLC_CONNECT_HOST, DEFAULT_VLC_PORT)
+        total_duration = get_video_duration(host, port)
         return random.randint(0, total_duration)
 
     return None
@@ -168,25 +166,9 @@ async def connect_to_server(client_type, auth_token, server_url):
 
     return True
 
-def signal_handler(sig, frame):
-    global VLC_PROCESS, DEFAULT_VLC_CONNECT_HOST, DEFAULT_VLC_PORT
-    print("Signal received, shutting down...")
-    if VLC_PROCESS:
-        try:
-            VLC_PROCESS.terminate()
-            print("VLC process terminated.")
-        except Exception as e:
-            print(f"Error stopping VLC: {e}")
-    sys.exit(0)
-
 
 async def main():
     global DEFAULT_VLC_ADVERTISE_HOST, DEFAULT_VLC_CONNECT_HOST, DEFAULT_VLC_PORT, VLC_PROCESS
-
-    # Register the signal handler
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
-
 
     parser = argparse.ArgumentParser(description='WebSocket Client')
     parser.add_argument('--type', choices=['seeker', 'switcher'], default=DEFAULT_CLIENT_TYPE,
